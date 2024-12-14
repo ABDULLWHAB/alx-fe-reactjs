614ff09982f7b99155b185eba674ccb6e63b3cae
ABDULWEHAB ABDELA <abdullahabdullwhab7@gmail.com>
2024-12-14 18:41:26 +0300

    formikform

---
+++ form-handling-react/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import formikForm from "./components/formikForm";
+
+const App = () => {
+  return (
+    <div>
+      <h1>User Registration</h1>
+      <formikForm />
+    </div>
+  );
+};
+
+export default App;
