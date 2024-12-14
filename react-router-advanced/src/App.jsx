ca93878916ea98d57c0cd7079f02c808e7deb61e
ABDULWEHAB ABDELA <abdullahabdullwhab7@gmail.com>
2024-12-14 21:04:48 +0300

    blog

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -4,6 +4,8 @@ import Profile from './pages/Profile';
 import BlogPost from './pages/BlogPost';
 import NotFound from './pages/NotFound';
 import ProtectedRoute from './components/ProtectedRoute';
+import ProfileDetails from './pages/ProfileDetails';
+import ProfileSettings from './pages/ProfileSettings';
 
 function App() {
   const isAuthenticated = false;
@@ -19,8 +21,11 @@ function App() {
               <Profile />
             </ProtectedRoute>
           }
-        />
-        <Route path="/post/:id" element={<BlogPost />} />
+        >
+          <Route path="details" element={<ProfileDetails />} />
+          <Route path="settings" element={<ProfileSettings />} />
+        </Route>
+        <Route path="/blog/:id" element={<BlogPost />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
