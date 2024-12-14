fbedd95d1830c85f58c0a374173e4b5ef52727b9
ABDULWEHAB ABDELA <abdullahabdullwhab7@gmail.com>
2024-12-14 21:54:53 +0300

    3

--- tailwind-react-integration/tailwind.config.js
+++ tailwind-react-integration/tailwind.config.js
@@ -1,11 +1,14 @@
 
 export default {
-  content: [
+  purge: [
     "./src/**/*.{js,jsx,ts,tsx}",
     "./public/index.html",
   ],
   darkMode: 'class',
   theme: {
+    extend: {},
+  },
+  variants: {
     extend: {},
   },
   plugins: [],
